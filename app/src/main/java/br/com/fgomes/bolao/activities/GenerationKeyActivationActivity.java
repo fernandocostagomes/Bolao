1d1d5de5ac7bb787b4b1abb1bcbc51edad50a17f
fernando.gomes <fernandocostagomes@gmail.com>
2017-11-23 12:10:17 -0200

    inicial compilacao.

---
+++ app/src/main/java/br/com/fgomes/bolao/activities/GenerationKeyActivationActivity.java
@@ -0,0 +1,15 @@
+package br.com.fgomes.bolao.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.fgomes.bolao.R;
+
+public class GenerationKeyActivationActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_generation_key_activation);
+   }
+}
