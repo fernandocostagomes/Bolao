280241788baf98e6c6a958cd0253f1229ad06f36
fernando.gomes <fernandocostagomes@gmail.com>
2018-01-03 16:56:06 -0200

    Insert a variable.

--- app/src/main/java/br/com/fgomes/bolao/objects/Activation.java
+++ app/src/main/java/br/com/fgomes/bolao/objects/Activation.java
@@ -9,63 +9,72 @@ import java.util.*;
 public class Activation {
 
    /** Id Activation */
-   public int m_ACT_Id_Activation;
+   public int idActivation;
    /** Name Activation*/
-   public CharSequence m_ACT_Name_Activation;
+   public CharSequence nameActivation;
    /** Key Activation*/
-   public CharSequence m_ACT_Key_Activation;
+   public CharSequence keyActivation;
    /** Date Generation Key*/
-   public Date m_ACT_Date_Gen_Activation;
+   public Date dateActivation;
    /** Date Validate Key*/
-   public Date m_ACT_Date_Val_Activation;
+   public Date dateValActivation;
    /** Status Activation */
-   public int m_ACT_Status_Activation;
+   public int statusActivation;
 
-   public int getM_ACT_Id_Activation() {
-      return m_ACT_Id_Activation;
+   public int getIdActivation() {
+      return idActivation;
    }
 
-   public void setM_ACT_Id_Activation(int m_ACT_Id_Activation) {
-      this.m_ACT_Id_Activation = m_ACT_Id_Activation;
+   public void setIdActivation(int idActivation) {
+      this.idActivation = idActivation;
    }
 
-   public CharSequence getM_ACT_Name_Activation() {
-      return m_ACT_Name_Activation;
+   public CharSequence getNameActivation() {
+      return nameActivation;
    }
 
-   public void setM_ACT_Name_Activation(CharSequence m_ACT_Name_Activation) {
-      this.m_ACT_Name_Activation = m_ACT_Name_Activation;
+   public void setNameActivation(CharSequence nameActivation) {
+      this.nameActivation = nameActivation;
    }
 
-   public CharSequence getM_ACT_Key_Activation() {
-      return m_ACT_Key_Activation;
+   public CharSequence getKeyActivation() {
+      return keyActivation;
    }
 
-   public void setM_ACT_Key_Activation(CharSequence m_ACT_Key_Activation) {
-      this.m_ACT_Key_Activation = m_ACT_Key_Activation;
+   public void setKeyActivation(CharSequence keyActivation) {
+      this.keyActivation = keyActivation;
    }
 
-   public Date getM_ACT_Date_Gen_Activation() {
-      return m_ACT_Date_Gen_Activation;
+   public Date getDateActivation() {
+      return dateActivation;
    }
 
-   public void setM_ACT_Date_Gen_Activation(Date m_ACT_Date_Gen_Activation) {
-      this.m_ACT_Date_Gen_Activation = m_ACT_Date_Gen_Activation;
+   public void setDateActivation(Date dateActivation) {
+      this.dateActivation = dateActivation;
    }
 
-   public Date getM_ACT_Date_Val_Activation() {
-      return m_ACT_Date_Val_Activation;
+   public Date getDateValActivation() {
+      return dateValActivation;
    }
 
-   public void setM_ACT_Date_Val_Activation(Date m_ACT_Date_Val_Activation) {
-      this.m_ACT_Date_Val_Activation = m_ACT_Date_Val_Activation;
+   public void setDateValActivation(Date dateValActivation) {
+      this.dateValActivation = dateValActivation;
    }
 
-   public int getM_ACT_Status_Activation() {
-      return m_ACT_Status_Activation;
+   public int getStatusActivation() {
+      return statusActivation;
    }
 
-   public void setM_ACT_Status_Activation(int m_ACT_Status_Activation) {
-      this.m_ACT_Status_Activation = m_ACT_Status_Activation;
+   public void setStatusActivation(int statusActivation) {
+      this.statusActivation = statusActivation;
+   }
+
+   public Activation(int idActivation, CharSequence nameActivation, CharSequence keyActivation, Date dateActivation, Date dateValActivation, int statusActivation) {
+      this.idActivation = idActivation;
+      this.nameActivation = nameActivation;
+      this.keyActivation = keyActivation;
+      this.dateActivation = dateActivation;
+      this.dateValActivation = dateValActivation;
+      this.statusActivation = statusActivation;
    }
 }
